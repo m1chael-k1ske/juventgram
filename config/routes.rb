0b9c4854fc5339bb16ffb428a82639c3ad93c0a2
m1chael-k1ske <xxxxxk1skexxxxx@yahoo.co.jp>
2021-01-27 14:13:04 +0000

    Vidal

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   
-  get 'pages/index' # path
+  get 'sessions/new'
   root 'pages#index' # controller#action
   get 'pages/help' # path
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  resources :users # まとめて定義
+  get    '/login',   to: 'sessions#new' # ログインするためのフォームを表示するページを取得 = GET /URL sessions_controller_new_action
+  post   '/login',   to: 'sessions#create'# メールアドレス, パスワードを送信してもらい, データを元にSessionを作成 = POST /URL sessions_controller_create_action
+  delete '/logout',  to: 'sessions#destroy'# ログアウトでSessionを削除 = DELETE /URL sessions_controller_destroy_action
 end
