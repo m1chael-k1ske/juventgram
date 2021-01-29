60c7a122648a0e4c7e3717e00518262b4d0ac581
m1chael-k1ske <xxxxxk1skexxxxx@yahoo.co.jp>
2021-01-29 14:40:53 +0000

    Boksic

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   resources :users # まとめて定義
-  get    '/login',   to: 'sessions#new' # ログインするためのフォームを表示するページを取得 = GET /URL sessions_controller_new_action
-  post   '/login',   to: 'sessions#create'# メールアドレス, パスワードを送信してもらい, データを元にSessionを作成 = POST /URL sessions_controller_create_action
-  delete '/logout',  to: 'sessions#destroy'# ログアウトでSessionを削除 = DELETE /URL sessions_controller_destroy_action
+  get    '/login',   to: 'sessions#new' # ログインするためのフォームを表示するページを取得, 転送<GET /URL sessions_controller_new_action>
+  post   '/login',   to: 'sessions#create'# メールアドレス, パスワードを送信してもらい, データを元にSessionを作成, 処理を振り分け<POST /URL sessions_controller_create_action>
+  delete '/logout',  to: 'sessions#destroy'# ログアウトでSessionを削除に処理を振り分け<DELETE /URL sessions_controller_destroy_action>
 end
