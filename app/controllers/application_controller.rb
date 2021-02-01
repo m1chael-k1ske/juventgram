de65d844703bdc653e38ac046497f97cc72f990c
m1chael-k1ske <xxxxxk1skexxxxx@yahoo.co.jp>
2021-02-01 14:48:56 +0000

    Jarni

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception # クロスサイトリクエストフォージェリ<Railsのセキュリティ対策に必要な一文>
+
+  add_flash_types :success, :info, :warning, :danger # フラッシュメッセージのキーを許可
+  
+  def current_user # current_userはViewとControllerのどちらでも使用したいからhelperとcontrollerの両方にメソッドを指定
+    @current_user ||= User.find_by(id: session[:user_id]) # インスタンス変数がfalse(nil)の場合Userモデルから現在ログインしているユーザーidと一致する情報を代入
+  end
+  
+  def logged_in? # # logged_in?メソッドは現在のユーザーがログインしているかどうか判別するメソッド
+    !current_user.nil? # current_userがnilか判断
+  end
 end
