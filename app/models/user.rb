90b4928d4706fafa79b2929aed95cb774f2ecab2
m1chael-k1ske <xxxxxk1skexxxxx@yahoo.co.jp>
2021-02-05 08:31:22 +0000

    Gattuso

---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ApplicationRecord # ApplicationRecord<Railsの基礎的なクラス>を継承してUserというクラスを定義
+  validates :name, presence: true # カラム, 空欄ではないか判断
+  validates :email, presence: true # カラム, 空欄ではないか判断
+  
+  has_secure_password # has_secure_passwordを記述した事で, Userモデルでpasswordとpassword_confirmationの2つが使えるようになり, DB内のpassword_digestに保存, その後gemfileにgem'bcrypt'を記述インストール
+end
