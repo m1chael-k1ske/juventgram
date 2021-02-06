27736da927f3b8147679da9e8b5580bc4f623782
m1chael-k1ske <xxxxxk1skexxxxx@yahoo.co.jp>
2021-02-06 11:54:41 +0000

    Crespo

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -2,4 +2,6 @@ class Topic < ApplicationRecord
   validates :user_id, presence: true # カラム, 空欄ではないか判断 Userテーブルのデータを参照するため, あらかじめUserテーブルのuser_idを<外部キー = 他のテーブルのプライマリキー(カラム)> 
   validates :description, presence: true # カラム, 空欄ではないか判断
   validates :image, presence: true # カラム, 空欄ではないか判断
+  
+  belongs_to :user # アソシエーション それぞれのTopicは1つのUserを持つ
 end
