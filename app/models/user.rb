c8205fe6a3311f036cec0ceaef269777fb84edf6
m1chael-k1ske <xxxxxk1skexxxxx@yahoo.co.jp>
2021-02-06 11:55:13 +0000

    Gren

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord # ApplicationRecord<Railsの基礎的なクラス
   validates :email, presence: true # カラム, 空欄ではないか判断
   
   has_secure_password # has_secure_passwordを記述した事で, Userモデルでpasswordとpassword_confirmationの2つが使えるようになり, DB内のpassword_digestに保存, その後gemfileにgem'bcrypt'を記述インストール
+  
+  has_many :topics # アソシエーション 1つのUserは複数のTopicを持つ
 end
