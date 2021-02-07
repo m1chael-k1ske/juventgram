e26eb9391c5535a2a1cfc70da949aeaecf58cbe7
m1chael-k1ske <xxxxxk1skexxxxx@yahoo.co.jp>
2021-02-07 12:32:24 +0000

    Gilardino

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,12 +4,18 @@ class UsersController < ApplicationController #Railsの慣習でControllerは必
   end
   
   def create # createアクションでリソースを追加しsaveメソッドを使用(保存), Controllerで保存されたインスタンス変数は,そのControllerに紐づくViewに値が渡される
-    @user = User.new(name: params[:user][:name], email: params[:user][:email])
+    @user = User.new(user_params)
+    
     if @user.save
       redirect_to rooth_path, success: '登録が完了しました' # success = 緑
     else
       flash.now[:danger] = "登録に失敗しました" # danger = 赤
       render :new # 表示したいビュー(usersディレクトリ内のnew.html.erbが表示)
     end
+  end
+  
+  private # クラス外からは呼び出せない
+  def user_params # ストロングパラメーター
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
