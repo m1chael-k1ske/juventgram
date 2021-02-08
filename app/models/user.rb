12bcd71df1f1ce547ffd2fa31841065c43638938
m1chael-k1ske <xxxxxk1skexxxxx@yahoo.co.jp>
2021-02-08 15:08:07 +0000

    Van Basten

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,5 @@ class User < ApplicationRecord # ApplicationRecord<Railsの基礎的なクラス
   
   has_many :topics # アソシエーション 1つのUserは複数のTopicを持つ
   has_many :favorites # アソシエーション 1つのUserは複数のFavoriteを持つ
+  has_many :favorite_topics, through: :favorites, source: 'topic' # through(中間テーブル)を使うことで, ログインしているユーザーがいいねしているTopicを全て取得
 end
